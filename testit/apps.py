ab6ae5883734840c8d465d024c702dd3a2e18458
fatemeh-akhondi <fatemeh.akhondy84@gmail.com>
2025-07-16 15:26:50 +0330

    UPD: autocreate userprofile

--- testit/apps.py
+++ testit/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class TestitConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'testit'
+    
+    def ready(self):
+        import testit.signals
